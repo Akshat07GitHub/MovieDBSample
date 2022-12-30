0f68e995a61baf24047e602eee559a1f317ee942
Akshat07GitHub <121692753+Akshat07GitHub@users.noreply.github.com>
2022-12-30 20:57:18 +0100

    Init

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = {
+    mode : "development",
+    entry : "./index.js",
+    output : {
+        path : path.resolve(__dirname,"public"),
+        filename : "main.js"
+    },
+    target : "web",
+    devServer : {
+        port : "7000",
+        static : ["./public"],
+        open : true
+    },
+    resolve : {
+        extensions : ['.js','.jsx']
+    },
+    module : {
+        rules : [
+            {
+                test : /\.(js|jsx)$/,
+                exclude : /node_modules/,
+                use : "babel-loader"
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+}
